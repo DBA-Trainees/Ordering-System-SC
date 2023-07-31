23d042a827e5869bae1913a7e0a2efeb22719425
DBASYSTEM\Shan.Celis <Shan.Celis@dbaadvisory.com>
2023-07-31 16:56:43 +0800

    Uploaded image in food module

--- OrderingSystem/angular/src/app/foods/create-edit-food/create-edit-food-modal.component.ts
+++ OrderingSystem/angular/src/app/foods/create-edit-food/create-edit-food-modal.component.ts
@@ -24,14 +24,16 @@ import {
 export class CreateEditFoodModalComponent extends AppComponentBase
     implements OnInit {
     saving = false;
-    food: FoodDto = new FoodDto();
+    food = new FoodDto;
     categories: CategoryDto[] = [];
     types: TypeDto[] = [];
-    id: number = 0;
+    id: number;
     selectCategoryId: number = null;
     selectTypeId: number = null;
-    imageName: File;
-    img: string;
+    base64textString: any;
+    img: any;
+    imageFileName: string;
+    imgFileType: string;
 
     @Output() onSave = new EventEmitter<any>();
 
@@ -49,32 +51,49 @@ export class CreateEditFoodModalComponent extends AppComponentBase
         if(this.id){
             this._foodServiceProxy.get(this.id).subscribe((res) => {
                 this.food = res;
-                this.food.name = res.name;
                 this.selectCategoryId = this.food.categoryId;
                 this.selectTypeId = this.food.typeId;
             });
         }
         this._categoryServiceProxy.getAllCategories().subscribe((res) => {
             this.categories = res;
-        });
+        })
         this._typeServiceProxy.getAllTypes().subscribe((res) => {
             this.types = res;
-        });
+        })
     }
 
-    imageSelected(file) {
+    /* imageSelected(file) {
         this.imageName = file.target.files[0];
         console.log(this.imageName.name);
         this.img = this.imageName.name
+    } */
+
+    imageFileSelected(event: any) : void {
+        var file = event.target.files[0];
+        var reader = new FileReader();        
+        if (file) {
+            
+            reader.onload = (res: any) => {
+                this.food.image = res.target.result.split(',')[1];
+                this.food.imageName = file.name;
+                this.food.imageFileType = file.filetype;
+            };
+            reader.readAsDataURL(file);
+        }
     }
+
 
 
     saveFood(): void {
         this.saving = true;
         this.food.categoryId = this.selectCategoryId;
         this.food.typeId = this.selectTypeId;
+       /*  this.food.image = this.base64textString;
+        this.food.imageName = this.imageFileName;
+        this.food.imageFileType = this.imgFileType; */
 
-        if(this.id !==0){
+        if(this.id > 0){
             this._foodServiceProxy.update(this.food).subscribe(
                 () => {
                     this.notify.info(this.l('SavedSuccessfully'));
